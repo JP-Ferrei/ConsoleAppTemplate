8009a8e75974e4f4461234af810f89b2100173ef
joao.pedrocunha <joao.pedrocunha@imusica.com.br>
2023-07-22 16:07:45 -0300

    first commit

---
+++ ConsoleAppTemplate/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleAppTemplate;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .WriteTo.File("logs/Logs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+var logger = Log.Logger;
+
+try
+{
+    logger.Information("Application starting");
+
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Configuration
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile($"appsettings.{builder.Configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT")}.json", true, true)
+        .AddCommandLine(args)
+        .AddEnvironmentVariables();
+
+    DependencyInjection.Inject(builder.Services, builder.Configuration);
+
+    builder.Services
+        .AddLogging(configure => configure.AddSerilog(Log.Logger))
+        .AddSingleton(Log.Logger)
+        .AddHostedService<Worker>()
+        .Configure<ConsoleLifetimeOptions>(opt => opt.SuppressStatusMessages = true);
+
+    var app = builder.Build();
+
+    await app.RunAsync();
+
+    app.WaitForShutdown();
+    app.Dispose();
+}
+catch (Exception ex)
+{
+    logger.Error("exception {excpetion}", ex);
+}
+logger.Information("Application is Shutting down");
---
+++ ConsoleAppTemplate/Worker.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ConsoleAppTemplate
+{
+    internal class Worker : BackgroundService
+    {
+        private readonly ILogger logger;
+
+        public Worker(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.Information("Start async");
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.Information("teste");
+        }
+
+        public override Task StopAsync(CancellationToken token)
+        {
+            logger.Information("stopAsync");
+            return base.StopAsync(token);
+        }
+
+        public override void Dispose()
+        {
+            logger.Information("Dispose");
+            base.Dispose();
+        }
+    }
+}
